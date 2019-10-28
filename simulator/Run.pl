45c8347daf3650a4054f35708c95a51cef398bd9
igenovese <ignacio.genovese@gmail.com>
2019-10-27 17:09:04 -0700

    Added combined branch predictor using two 2-level predictors

--- simulator/Run.pl
+++ simulator/Run.pl
@@ -31,7 +31,7 @@
 
 ###########################################################################
 ## hpux/linux/NT specific settings go up here
-$exp_dir = "/u/genovese/Documents/remote/ECE587/simulator";
+$exp_dir = "/u/genovese/Documents/remote/ECE587/ECE587_FP/simulator";
 $bench_dir = "$exp_dir/bench";
 $input_dir = "$exp_dir/input/ref";
 $output_dir = "$exp_dir/output/ref";
